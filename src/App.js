30a5e1d1c4fcd98073e3b64a5606e43694901a19
Hyoseok Seo <seohyoseok0408@gmail.com>
2025-03-13 19:24:38 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+
+  const [products, setProducts] = useState([]);
+
+  async function onLoad(){
+    let response = await fetch("/api/products");
+    let responseBody = await response.json();
+    console.log("onLoad", responseBody);
+    setProducts(responseBody)
+  }
+
+  // 초기화 메서드
+  useEffect(()=>{
+    onLoad();
+  }, []);
+
+  return (
+    <div>
+      <h1>상품목록페이지</h1>
+      <hr/>
+      <div>
+        {products.map((product)=> <Card product={product}/>)}
+      </div>
+    </div>
+  );
+}
+
+export default App;
