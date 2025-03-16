b76bb9af9ed2217b7781aee21e983b89d9eab8c6
yuchangseuk <ckdtjr428@naver.com>
2025-03-16 17:47:00 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Card from './components/Card';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-
   const [products, setProducts] = useState([]);
 
-  async function onLoad(){
+  async function onLoad() {
     let response = await fetch("/api/products");
     let responseBody = await response.json();
     console.log("onLoad", responseBody);
-    setProducts(responseBody)
+    setProducts(responseBody);
   }
 
   // 초기화 메서드
-  useEffect(()=>{
+  useEffect(() => {
     onLoad();
   }, []);
 
   return (
     <div>
-      <h1>상품목록페이지~~!!!</h1>
-      <hr/>
+      <h1>오징어어목록페이지~~!!!</h1>
+      <hr />
       <div>
-        {products.map((product)=> <Card product={product}/>)}
+        {products.map((product) => (
+          <Card product={product} />
+        ))}
       </div>
     </div>
   );
